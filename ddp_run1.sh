d48f32c2a7a89daeb805135d26640dd9d2129362
lankuohsing <lankuohsing@163.com>
2025-06-13 10:58:22 +0800

    change cuda_visible_devices

--- ddp_run1.sh
+++ ddp_run1.sh
@@ -10,10 +10,13 @@ export NCCL_IB_DISABLE=1
 export NCCL_SOCKET_IFNAME=bond0  # 替换为你的实际接口
 export NCCL_BLOCKING_WAIT=1
 
+# GPU可视设置
+export CUDA_VISIBLE_DEVICES=0,1
+
 # 显示网络信息
 echo "Worker starting at $(hostname) - $(date)"
-ip addr
-ifconfig
+# ip addr
+# ifconfig
 
 # 启动训练
 torchrun \
