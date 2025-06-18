4e05f5d590814c6e45331b6f55e5374c2a3a5141
lankuohsing <lankuohsing@163.com>
2025-06-18 14:21:24 +0800

    update network

--- torch_official_examples/torchrun0.sh
+++ torch_official_examples/torchrun0.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+export GLOO_SOCKET_IFNAME=bond0  # 使用bond0接口
+export NCCL_SOCKET_IFNAME=bond0   # 使用bond0接口
+
 CUDA_VISIBLE_DEVICES=4,5 torchrun \
 --nproc_per_node=2 \
 --nnodes=2 \
@@ -5,4 +9,5 @@ CUDA_VISIBLE_DEVICES=4,5 torchrun \
 --rdzv_id=456 \
 --rdzv_backend=c10d \
 --rdzv_endpoint=10.205.92.13:29603 \
+--rdzv_conf is_host_name=false \
 multinode.py 50 10 2>&1 | tee master.log
--- torch_official_examples/torchrun1.sh
+++ torch_official_examples/torchrun1.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+export GLOO_SOCKET_IFNAME=bond0  # 使用bond0接口
+export NCCL_SOCKET_IFNAME=bond0   # 使用bond0接口
 CUDA_VISIBLE_DEVICES=0,1 torchrun \
 --nproc_per_node=2 \
 --nnodes=2 \
@@ -5,4 +8,5 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun \
 --rdzv_id=456 \
 --rdzv_backend=c10d \
 --rdzv_endpoint=10.205.92.13:29603 \
+--rdzv_conf is_host_name=false \  # 强制使用IP
 multinode.py 50 10 2>&1 | tee worker.log
