a912e9c8e296ad1c6d32be8b1c38f938f732ca86
lankuohsing <lankuohsing@163.com>
2025-06-18 14:50:28 +0800

    add ipv4 config

--- torch_official_examples/torchrun1.sh
+++ torch_official_examples/torchrun1.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-export TORCH_DISTRIBUTED_PORT_RANGE="29500-30000"
 export GLOO_SOCKET_IFNAME=bond0
 export NCCL_SOCKET_IFNAME=bond0
+
 # 强制使用 IPv4
 export GLOO_SOCKET_FAMILY=IPv4
 export NCCL_P2P_DISABLE=1
+
+# 设置分布式端口范围
+export TORCH_DISTRIBUTED_PORT_RANGE="29500-30000"
+
+# 设置日志级别
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+export NCCL_DEBUG=INFO
+
 CUDA_VISIBLE_DEVICES=0,1 torchrun \
 --nproc_per_node=2 \
 --nnodes=2 \
